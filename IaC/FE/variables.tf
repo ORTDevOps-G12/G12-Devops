7856fc050bf3ba242209379fc8b93398aab7762c
NConand <130004239+NConand@users.noreply.github.com>
2024-07-17 20:54:37 -0300

    FE files

---
+++ IaC/FE/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+    description = "Default aws region"
+    type = string
+}
+
+variable "build_artifact_path" {
+  description = "The path to the build artifact"
+  type        = string
+}
+
+variable "project_name" {
+    description = "Default project name"
+    type = string
+}
